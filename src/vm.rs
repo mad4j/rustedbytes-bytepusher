4948ed89378d9598f874a01c085047875b34f47f
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-06-08 17:38:22 +0200

    fixes for publishing

--- src/vm.rs
+++ src/vm.rs
@@ -24,6 +24,8 @@ pub const PROGRAM_COUNTER_ADDR: usize = 0x000002;
 pub const SCREEN_REGISTER_ADDR: usize = 0x000005;
 pub const AUDIO_REGISTER_ADDR: usize = 0x000006;
 
+/// VirtualMachine struct encapsulates the components of the BytePusher VM.
+/// It includes the CPU, memory, audio handler, keyboard handler, and screen handler.
 pub struct VirtualMachine {
     pub _window: Rc<RefCell<Window>>,
     pub _sink: Rc<RefCell<Sink>>,
@@ -35,23 +37,39 @@ pub struct VirtualMachine {
     pub frame_duration: Duration,
 }
 
+
+/// Implementation of the VirtualMachine
+/// This struct encapsulates the VM's components and provides methods to load ROMs and process frames.
+/// It includes the CPU, memory, audio handler, keyboard handler, and screen handler.
 impl VirtualMachine {
     pub fn new(
         window: Rc<RefCell<Window>>,
         sink: Rc<RefCell<Sink>>,
         frame_duration: Duration,
     ) -> Self {
+
+        // Initialize the memory with a size of MEMORY_SIZE
         let memory = Rc::new(RefCell::new(Memory::new(MEMORY_SIZE)));
+        
+        // Initialize the CPU with the memory
         let cpu = Cpu::new(Rc::clone(&memory));
+        
+        // Initialize the audio handler with the memory and sink
         let audio_handler =
             AudioHandler::new(AUDIO_REGISTER_ADDR, Rc::clone(&memory), Rc::clone(&sink));
+        
+        // Initialize the keyboard handler with the memory and window
         let keyboard_handler = KeyboardHandler::new(
             KEYBOARD_REGISTER_ADDR,
             Rc::clone(&window),
             Rc::clone(&memory),
         );
+
+        // Initialize the screen handler with the memory and window
         let screen_handler =
             ScreenHandler::new(SCREEN_REGISTER_ADDR, Rc::clone(&memory), Rc::clone(&window));
+        
+        // Return the new VirtualMachine instance
         Self {
             _window: window,
             _sink: sink,
@@ -64,31 +82,40 @@ impl VirtualMachine {
         }
     }
 
+    /// Load a ROM file into the VM's memory.
     pub fn load_rom(&mut self, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
         let rom_data = std::fs::read(file_path)?;
         self.memory.borrow_mut().copy_from(0, &rom_data);
         Ok(())
     }
 
+    /// Process a single frame of the VM, handling input, CPU ticks, audio, and rendering.
     pub fn process_frame(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+
+        // Measure the time taken for the frame processing
         let start = Instant::now();
 
+        // Handle keyboard events
         self.keyboard_handler.handle_events();
 
+        // process the CPU instructions
         self.cpu.tick();
 
+        // Append audio samples from the memory to the audio sink
         self.audio_handler.append_samples();
 
+        // Render the screen frame
         self.screen_handler.render_frame()?;
 
+        // wait for the next frame duration
+        // This is to ensure that each frame takes approximately the same amount of time
         let elapsed = start.elapsed();
-
-        //TODO: usare invece il limitatore di minifb
         if elapsed < self.frame_duration {
             std::thread::sleep(self.frame_duration - elapsed);
         } else {
             eprintln!("Frame took too long: {:?}", elapsed);
         }
+
         Ok(())
     }
 }
